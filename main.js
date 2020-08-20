eb96a1278d33509199c0fc77ecc83712c1dc17cf
Hayes Choy <hayeselnut2127@gmail.com>
2020-08-21 02:55:09 +1000

    tried to impleent delete - bigger problems encountered

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const SPOTIFY_SECRET = "YWE2MDFlZTRkYzYyNDY2Mzk3Zjg1OTlkZTQ5NDQ2NDg6NzUyNjRmZGYzN2FjNDNiZGJiZjJkZTk0M2I3ZTM2Njc="
+const SPOTIFY_CLIENT_ID = "aa601ee4dc62466397f8599de4944648";
+const SPOTIFY_SECRET = "YWE2MDFlZTRkYzYyNDY2Mzk3Zjg1OTlkZTQ5NDQ2NDg6NzUyNjRmZGYzN2FjNDNiZGJiZjJkZTk0M2I3ZTM2Njc=";
+
 const GRANT_TYPE = "client_credentials";
+const MODIFY_PLAYLIST_SCOPE = ["playlist-modify-public", "playlist-modify-private"];
 
 var ACCESS_TOKEN;
+var MODIFY_TOKEN;
 
-$(document).ready(function() {
+// RANDOM STRING: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+var STATE = 123;
+
+function clientCredentialsFlow() {
     $.ajax({
         type: "POST",
         url: "https://accounts.spotify.com/api/token",
@@ -17,6 +24,45 @@ $(document).ready(function() {
             alert("Could not connect to Spotify API");
         }
     });
+}
+
+function implicitGrantFlow() {
+
+    var current_site = encodeURIComponent("http://localhost:5501/");
+    var actual_site = encodeURIComponent("https://hayeselnut.github.io/deduplicatify/index.html");
+
+    var redirect_url = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=token&redirect_uri=${current_site}&state=${STATE}&scope=${encodeURIComponent(MODIFY_PLAYLIST_SCOPE.join(" "))}`
+
+    // Redirect them to redirect_url
+    window.location.replace(redirect_url);
+
+    // NOTHING HERE WILL GET RUN, ALL PROCESSES KILLED
+
+
+
+
+
+    // They should get redirected back to same page (or should i do different page)
+
+    // Then retrieve hash from # data area.
+}
+
+$(document).ready(function() {
+    clientCredentialsFlow();
+
+    if (location.hash) {
+        var hash = location.hash.substring(1);
+        MODIFY_TOKEN = hash.split("&")[0].split("=")[1];
+        var state = hash.split("&")[3].split("=")[1]
+
+        if (state != STATE) {
+            alert("STATE IS NOT" + state + "=/=" + STATE);
+        }
+
+        // Do things with MODIFY_TOKEN
+        // NOTE ALL MY DATA IS LOST :((
+    }
+
 });
 
 function getPlaylistId(playlistLink) {
@@ -31,35 +77,6 @@ function getPlaylistId(playlistLink) {
         return playlistLink.split(":")[2];
     }
 }
-
-function updatePlaylistMetadata(p) {
-    // p = playlist_object
-
-    var duration = 500;
-
-    $("#playlist-metadata").fadeOut(duration, function() {
-        $("#playlist-name").html(p.name);
-        $("#playlist-owner").html(p.owner.display_name);
-        $("#playlist-desc").html(p.description);
-        $("#playlist-length").html(p.tracks.total + " song");
-
-        if (p.tracks.total > 1) {
-            $("#playlist-length").append("s");
-        }
-
-        if (p.images.length) {
-            $("#playlist-img").attr("src", p.images[0].url);
-        } else {
-            $("#playlist-img").attr("src", "assets/blank-playlist.jpg");
-        }
-    }).fadeIn(duration, function() {
-        $("#sim-songs").css("padding-top", "0").fadeIn(duration);
-    });
-}
-
-// $("#dedup-txtbox").on("click", function() {
-//     $(this).val("");
-// });
 
 function getSongDetails(song) {
     const artists = [];
@@ -96,55 +113,6 @@ function getTracks(url) {
         url: url,
         headers: {"Authorization": "Bearer " + ACCESS_TOKEN},
     });
-}
-
-function isSimilarSong(song1, song2) {
-    const buzzwords = ["mix", "acoustic", "live"];
-    var s1 = song1.name.toLowerCase();
-    var s2 = song2.name.toLowerCase();
-
-    // Check song name is similar
-    if (s1.length <= 2 || s2.length <= 2) {
-        // Special case for small song names
-        if (s1 != s2) {
-            return false;
-        }
-    } else {
-        if (!s1.includes(s2) && !s2.includes(s1)) {
-            return false;
-        }
-    }
-
-    // Check artist ids
-    const intersection = song1.artists.filter(a => song2.artists.includes(a));
-    if (!intersection.length) {
-        return false;
-    }
-
-    // Check if songs are within 2.5 secs of each other
-    if (Math.abs(song1.duration_ms - song2.duration_ms) > 2500) {
-        return false;
-    }
-
-    // Check not a transformed version of original song
-    for (var i = 0; i < buzzwords.length; i++) {
-        var bw = buzzwords[i];
-
-        if ( (s1.includes(bw) && !s2.includes(bw)) || (s2.includes(bw) && !s1.includes(bw)) ) {
-            return false;
-        }
-    }
-
-    // They are similar!
-    console.log(Math.abs(song1.duration_ms - song2.duration_ms));
-    return true;
-}
-
-function printSong(song) {
-    // DEFAULT FOR COMPACT IS 300x80
-    var width = 300;
-    var height = 80;
-    return `<iframe src="https://open.spotify.com/embed/track/${song.id}" width=${width} height=${height} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
 }
 
 function getDuplicates(songs) {
@@ -230,6 +198,80 @@ function showDuplicates(songs, duplicates) {
     });
 }
 
+function updatePlaylistMetadata(playlist) {
+    // p = playlist_object
+
+    var duration = 500;
+
+    $("#playlist-metadata").fadeOut(duration, function() {
+        $("#playlist-name").html(playlist.name);
+        $("#playlist-owner").html(playlist.owner.display_name);
+        $("#playlist-desc").html(playlist.description);
+        $("#playlist-length").html(playlist.tracks.total + " song");
+
+        if (playlist.tracks.total > 1) {
+            $("#playlist-length").append("s");
+        }
+
+        if (playlist.images.length) {
+            $("#playlist-img").attr("src", playlist.images[0].url);
+        } else {
+            $("#playlist-img").attr("src", "assets/blank-playlist.jpg");
+        }
+    }).fadeIn(duration, function() {
+        $("#sim-songs").css("padding-top", "0").fadeIn(duration);
+    });
+}
+
+function isSimilarSong(song1, song2) {
+    const buzzwords = ["mix", "acoustic", "live"];
+    var s1 = song1.name.toLowerCase();
+    var s2 = song2.name.toLowerCase();
+
+    // Check song name is similar
+    if (s1.length <= 2 || s2.length <= 2) {
+        // Special case for small song names
+        if (s1 != s2) {
+            return false;
+        }
+    } else {
+        if (!s1.includes(s2) && !s2.includes(s1)) {
+            return false;
+        }
+    }
+
+    // Check artist ids
+    const intersection = song1.artists.filter(a => song2.artists.includes(a));
+    if (!intersection.length) {
+        return false;
+    }
+
+    // Check if songs are within 2.5 secs of each other
+    if (Math.abs(song1.duration_ms - song2.duration_ms) > 2500) {
+        return false;
+    }
+
+    // Check not a transformed version of original song
+    for (var i = 0; i < buzzwords.length; i++) {
+        var bw = buzzwords[i];
+
+        if ( (s1.includes(bw) && !s2.includes(bw)) || (s2.includes(bw) && !s1.includes(bw)) ) {
+            return false;
+        }
+    }
+
+    // They are similar!
+    console.log(Math.abs(song1.duration_ms - song2.duration_ms));
+    return true;
+}
+
+function printSong(song) {
+    // DEFAULT FOR COMPACT IS 300x80
+    var width = 300;
+    var height = 80;
+    return `<iframe src="https://open.spotify.com/embed/track/${song.id}" width=${width} height=${height} frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+}
+
 $("#dedup-txtbox").on("keydown", function(event) {
     if (event.which == 13) { // Enter key
         $("#dedup-btn").click();
@@ -283,3 +325,13 @@ $("#dedup-btn").on("click", async function() {
         $("#remove-dupes-btn").delay(1000).fadeIn(1000);
     }
 });
+
+$("#remove-dupes-btn").on("click", async function() {
+    implicitGrantFlow();
+
+    // Remove duplicates
+
+    // If already have user token, try that
+
+    // If fails or don't have user token, request for authorisation
+})
